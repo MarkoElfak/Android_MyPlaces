2267124015d18e7d1271da980fe86454abc6cf93
MareVR <marko.max.ilic97@gmail.com>
2021-07-19 01:28:16 +0200

    Lab5 commit, firebase

--- app/src/main/java/rs/elfak/mosis/markoilic/myplaces/EditMyPlaceActivity.java
+++ app/src/main/java/rs/elfak/mosis/markoilic/myplaces/EditMyPlaceActivity.java
@@ -55,9 +55,9 @@ public class EditMyPlaceActivity extends AppCompatActivity implements View.OnCli
         } else if (position>=0) {
             finishedButton.setText("Save");
             MyPlace place = MyPlacesData.getInstance().getPlace(position);
-            nameEditText.setText(place.getName());
+            nameEditText.setText(place.name);
             EditText descEditText = (EditText)findViewById(R.id.editmyplace_desc_edit);
-            descEditText.setText(place.getDesc());
+            descEditText.setText(place.description);
         }
 
         finishedButton.setOnClickListener(this);
@@ -105,15 +105,16 @@ public class EditMyPlaceActivity extends AppCompatActivity implements View.OnCli
                 String lon = lonEdit.getText().toString();
                 if(!editMode) {
                     MyPlace place = new MyPlace(nme, desc);
-                    place.setLatitude(lat);
-                    place.setLongitude(lon);
+                    place.latitude = lat;
+                    place.longitude = lon;
                     MyPlacesData.getInstance().addNewPlace(place);
                 } else {
-                    MyPlace place = MyPlacesData.getInstance().getPlace(position);
-                    place.setName(nme);
-                    place.setDescription(desc);
-                    place.setLongitude(lon);
-                    place.setLatitude(lat);
+//                    MyPlace place = MyPlacesData.getInstance().getPlace(position);
+//                    place.setName(nme);
+//                    place.setDescription(desc);
+//                    place.setLongitude(lon);
+//                    place.setLatitude(lat);
+                    MyPlacesData.getInstance().updatePlace(position, nme, desc, lon, lat);
                 }
                 setResult(Activity.RESULT_OK);
                 finish();
--- app/src/main/java/rs/elfak/mosis/markoilic/myplaces/MyPlace.java
+++ app/src/main/java/rs/elfak/mosis/markoilic/myplaces/MyPlace.java
@@ -2,37 +2,36 @@ package rs.elfak.mosis.markoilic.myplaces;
 
 import android.view.View;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class MyPlace {
-    String name;
-    String description;
-    String longitude;
-    String latitude;
-    int ID;
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setId(int ID){
-        this.ID = ID;
-    }
+    public String name;
+    public String description;
+    public String longitude;
+    public String latitude;
+    @Exclude
+    public String key;        // key dodat od strane Firebase pa ga Excludujemo/IgnoreExtra
+    public MyPlace(){}
 
 
-    public String getLongitude(){
-        return longitude;
-    }
 
-    public void setLongitude(String longitude){
-        this.longitude = longitude;
-    }
-
-    public String getLatitude(){
-        return latitude;
-    }
-
-    public void setLatitude(String latitude){
-        this.latitude = latitude;
-    }
+//    public String getLongitude(){
+//        return longitude;
+//    }
+//
+//    public void setLongitude(String longitude){
+//        this.longitude = longitude;
+//    }
+//
+//    public String getLatitude(){
+//        return latitude;
+//    }
+//
+//    public void setLatitude(String latitude){
+//        this.latitude = latitude;
+//    }
 
 
     public MyPlace(String nme, String desc) {
@@ -44,18 +43,18 @@ public class MyPlace {
         this(nme,"");
     }
 
-    public String getName(){
-        return name;
-    }
-    public String getDesc(){
-        return description;
-    }
-    public void setName(String nme){
-        this.name = nme;
-    }
-    public void setDescription(String desc){
-        this.description = desc;
-    }
+//    public String getName(){
+//        return name;
+//    }
+//    public String getDesc(){
+//        return description;
+//    }
+//    public void setName(String nme){
+//        this.name = nme;
+//    }
+//    public void setDescription(String desc){
+//        this.description = desc;
+//    }
 
     @Override
     public String toString(){
